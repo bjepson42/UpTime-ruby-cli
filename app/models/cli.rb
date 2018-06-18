54ef5acb17c2af5ee88fc0773e33d4d7b3acf601
orenmagid <autoimpedicis@gmail.com>
2018-06-18 19:40:54 -0400

    cli modifications and db/migrate/003_create_activities.rb modifications

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class Cli
-  attr_accessor :user, :possibility
+  attr_accessor :user, :possibility, :user_time, :accepted_or_rejected
   def call
      self.start
   end
@@ -25,27 +25,63 @@ class Cli
     puts "2. 30 minutes"
     puts "3. 60 minutes"
     puts "Please enter a number."
-    user_time = gets.strip
+    self.user_time = gets.strip
     if user_time == "exit"
       exit
     end
+    self.suggest_possibility
+  end
 
-    #<----pull this out and make a helper method so we can call it if rejected
-    case user_time
+  def suggest_possibility
+    case self.user_time
     when "1"
       self.possibility = self.user.suggest_random_possibility(15)
+      puts self.possibility.name
+      puts self.possibility.description
+      self.accept_or_reject
     when "2"
       self.possibility =  self.user.suggest_random_possibility(30)
+      puts self.possibility.name
+      puts self.possibility.description
+      self.accept_or_reject
     when "3"
       self.possibility = self.user.suggest_random_possibility(60)
+      puts self.possibility.name
+      puts self.possibility.description
+      self.accept_or_reject
     else
-      puts "Please enter a number."
+      self.how_much_time?
     end
-
   end
+
 
 #------Accept or reject, save as Activity regardless
 
+  def accept_or_reject
+    puts "Would you like to accept or reject this possibility?"
+    puts "1. Accept this possibility"
+    puts "2. Reject this possibility. Suggest another."
+    self.accepted_or_rejected = gets.strip
+    self.create_activity
+  end
+
 #------after accept don't autoquit ask if they want another activity or to quit
+
+  def create_activity
+
+    if self.accepted_or_rejected == "1"
+      activity = Activity.create(status: "accepted", name: self.possibility.name, description: self.possibility.description, physical_intensity: self.possibility.physical_intensity, mental_intensity: self.possibility.mental_intensity, fun_index: self.possibility.fun_index, duration_in_minutes: self.possibility.duration_in_minutes, necessary_location: self.possibility.necessary_location, user_id: self.user.id, possibility_id: self.possibility.id)
+      
+    elsif self.accepted_or_rejected == "2"
+      activity = Activity.create(status: "rejected", name: self.possibility.name, description: self.possibility.description, physical_intensity: self.possibility.physical_intensity, mental_intensity: self.possibility.mental_intensity, fun_index: self.possibility.fun_index, duration_in_minutes: self.possibility.duration_in_minutes, necessary_location: self.possibility.necessary_location, user_id: self.user.id, possibility_id: self.possibility.id)
+
+      puts "Suggesting another possibility..."
+      self.suggest_possibility
+
+    end
+
+
+
+  end
 
 end
--- db/migrate/003_create_activities.rb
+++ db/migrate/003_create_activities.rb
@@ -3,6 +3,14 @@ class CreateActivities < ActiveRecord::Migration[4.2]
   def change
     create_table :activities do |t|
       t.string :status #accepted/done or rejected (I think marking it as already done might be something for later, but let me know)
+      t.string :name
+      t.string :description
+      t.integer :physical_intensity #1-5
+      t.integer :mental_intensity #1-5
+      t.integer :fun_index #1-5
+      t.integer :duration_in_minutes
+      t.string :necessary_location #I occured to me that we can just indicate a necessary location (work or home) and leave this nil if no location is necessary for the possibility
+      t.boolean :others_required #I've set this up as a boolean, just to indicate if others are required or not. I think in the simple, first version of this, that might be all we should do.
       t.integer :user_id
       t.integer :possibility_id
       t.integer :rating
