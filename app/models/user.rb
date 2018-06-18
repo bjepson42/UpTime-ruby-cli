69c7ffceb6b76be1cc4cbac5185de5a203ce2a0c
orenmagid <autoimpedicis@gmail.com>
2018-06-18 16:17:12 -0400

    add notes to methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,15 @@ class User < ActiveRecord::Base
   has_many :possibilities, through: :activities
 
   def suggest_random_possibility
+    #call method of same name from Possibility class
 
   end
 
 
 
-  def create_and_save_activity
+  def create_and_save_activity(user_response)
+    #two options: accept or reject, change status appropriately
+    #if rejected, call method again
 
   end
 
