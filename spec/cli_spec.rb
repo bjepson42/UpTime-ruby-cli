23b763de537db12f7290157ea1411922e514a69a
github username <github email address>
2018-06-20 18:06:23 -0400

    Working on Rspec Stub

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,7 +2,7 @@ require_relative "../app/models/cli.rb"
 require "spec_helper"
 
 describe "Cli" do
-  let(:cli) {Cli.new} #<---this lets you use 'cli' as a universal new object
+  let(:cli) { Cli.new } #<---this lets you use 'cli' as a universal new object
 
 
   # it "stuff the test is checking for" do
@@ -18,10 +18,18 @@ describe "Cli" do
   #   expect(greeting.name).to eq "Brian"
   # end
 
+
+  # it "responds to 'exit'" do
+  #   self.stub(:gets).and_return("exit")
+  #   exit_output = capture_stdout { run(songs) }
+  #   expect(exit_output).to include("Goodbye")
+  # end
+
 #------this should check to see if a function calls self.quit if it receives "quit"
-  it "exit should quit program" do
-    allow(cli.where_are_you_at?).to receive(:gets).and_return("quit")
-    self.quit
+  it "quit input should exit program" do
+    self.stub(:gets) {"quit"}
+    #cli.where_are_you_at?.stub(:gets).and_return("quit")
+    expect { cli.where_are_you_at? }.to output(/Thank you for using UPTIME!/).to_stdout
     #expect { cli.where_are_you_at? }.to output(/Thank you for using UPTIME!/).to_stdout
   end
 
