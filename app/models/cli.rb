d2ca9ef208021df2fb9eb33936d6993642d786cd
github username <github email address>
2018-06-20 13:11:22 -0400

    added quit option for all user_input

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -13,28 +13,32 @@ class Cli
     puts ""
     puts Rainbow("Welcome to UPTIME!").bright.underline
     puts ""
+    puts "You may type 'quit' at any time to leave UPTIME."
+    puts ""
     puts "Have you used UPTIME on this computer before?"
     puts ""
-    puts "1. Yes"
-    puts "2. No"
+    puts " 1. Yes"
+    puts " 2. No"
     puts ""
     used_before = gets.strip
+    self.quit(used_before)
     if used_before == "1"
       self.user_already_exists
     elsif used_before == "2"
       self.create_new_user
+    else
+      self.what_was_that?
+      self.start
     end
-
   end
 
 
   def user_already_exists
     puts ""
-    puts ""
     puts "Please type your name (first and last)."
     puts ""
-    puts ""
     user_name_response = gets.chomp
+    self.quit(user_name_response)
     user_name_array = user_name_response.split(" ")
     self.user = User.find_by(first_name: user_name_array[0], last_name: user_name_array[1])
     if self.user == nil
@@ -63,39 +67,36 @@ class Cli
       puts ""
       puts "We could not find you in our records. Are you sure you've used UPTIME on this computer before?"
       puts ""
-      puts "1. Yes. Perhaps I mispelled my name. I'll try again."
-      puts "2. No. I better tell you more about myself."
+      puts " 1. Yes. Perhaps I mispelled my name. I'll try again."
+      puts " 2. No. I better tell you more about myself."
       double_check_used_before = gets.strip
+      self.quit(double_check_used_before)
       if double_check_used_before == "1"
         self.user_already_exists
       elsif double_check_used_before == "2"
         self.create_new_user
+      else
+        self.what_was_that?
+        self.user_already_exists
       end
     end
   end
 
   def create_new_user
     puts ""
-    puts ""
     puts "Tell us a little about yourself. It will only take a moment."
     puts ""
-    puts ""
-    puts "What is your first name?"
-    new_user_first_name = gets.strip
-    puts ""
-    puts ""
-    puts "What is your last name?"
-    new_user_last_name = gets.strip
-    puts ""
+    puts "What is your name?"
+    new_user_full_name = gets.strip
+    self.quit(new_user_full_name)
     puts ""
     puts "Is there something you'd like to be called, other than your first name?"
     puts ""
-    puts ""
-    puts "1. Yes"
-    puts "2. No"
+    puts " 1. Yes"
+    puts " 2. No"
     nick_name = gets.strip
+    self.quit(nick_name)
     if nick_name == "1"
-      puts ""
       puts ""
       puts "What would you like us to call you? (In other words, what is your nickname?)"
       new_user_nickname = gets.chomp
@@ -104,22 +105,24 @@ class Cli
       puts Rainbow("Well hello, #{new_user_nickname}! Let's get started!").underline
     elsif nick_name == "2"
       puts ""
-      puts ""
-      puts Rainbow("Okay, great! We'll just call you #{new_user_first_name}. Let's get started!").underline
+      puts Rainbow("Okay, great! We'll just call you #{new_user_full_name.split(" ")[0]}. Let's get started!").underline
     end
-    self.user = User.create(first_name: new_user_first_name, last_name: new_user_last_name, nick_name: new_user_nickname)
-    self.user.where_are_you_at
+    self.user = User.create(first_name: new_user_full_name.split(" ")[0], last_name: new_user_full_name.split(" ")[1], nick_name: new_user_nickname)
+    self.where_are_you_at?
   end
 
 #------user sets location constraint->moves to time constraint
 
   def where_are_you_at?
     puts "Where are you?"
-    puts "1. Home"
-    puts "2. Work"
-    puts "3. Somewhere else"
+    puts ""
+    puts " 1. Home"
+    puts " 2. Work"
+    puts " 3. Somewhere else"
+    puts ""
     puts "Please enter a number."
     user_response = gets.strip
+    self.quit(user_response)
     if user_response == "1"
       self.limit_place = "home"
       self.how_much_time?
@@ -130,6 +133,7 @@ class Cli
       self.limit_place = "not work or home"
       self.how_much_time?
     end
+    puts ""
   end
 
 
@@ -140,22 +144,17 @@ class Cli
   def how_much_time?
     #how much time do you have 1. 15min, 2. 30 min, 3. 60 minutes
     puts ""
-    puts ""
     puts "About how much time do you have?"
     puts ""
-    puts ""
-    puts "1. 15 minutes"
-    puts "2. 30 minutes"
-    puts "3. 60 minutes"
+    puts " 1. 15 minutes"
+    puts " 2. 30 minutes"
+    puts " 3. 60 minutes"
     puts ""
     puts ""
     puts "Please enter a number."
     puts ""
-    puts ""
     self.user_time = gets.strip
-    if user_time == "exit"
-      exit
-    end
+    self.quit(self.user_time)
     self.suggest_possibility
   end
 
@@ -199,11 +198,12 @@ class Cli
     puts "Would you like to accept or reject this possibility?"
     puts ""
     puts ""
-    puts "1. Accept this possibility"
-    puts "2. Reject this possibility for now. Suggest another."
-    puts "3. Reject this possibility, and never show it to me again!"
+    puts " 1. Accept this possibility"
+    puts " 2. Reject this possibility for now. Suggest another."
+    puts " 3. Reject this possibility, and never show it to me again!"
     puts ""
     self.accepted_or_rejected = gets.strip
+    self.quit(self.accepted_or_rejected)
     self.create_activity
   end
 
@@ -219,34 +219,36 @@ class Cli
         puts ""
         puts "What should we call your new possibility?"
         self.new_possibility_name = gets.strip
+        self.quit if self.new_possibility_name == "quit"
         puts ""
         puts "Write a brief description of your new possibility."
         self.new_possibility_description = gets.strip
+        self.quit if self.new_possibility_description == "quit"
         puts ""
         puts "About how long should it take to complete this possibility?"
         puts ""
         puts ""
-        puts "1. 15 minutes"
-        puts "2. 30 minutes"
-        puts "3. 60 minutes"
+        puts " 1. 15 minutes"
+        puts " 2. 30 minutes"
+        puts " 3. 60 minutes"
         puts ""
         puts ""
         puts "Please enter a number."
         self.new_possibility_duration = gets.strip
+        self.quit if self.new_possibility_duration == "quit"
         puts ""
         puts "Is this possibility something I can do at home, at work, anywhere but work, or anywhere at all?"
         puts ""
-        puts ""
-        puts "1. I can only do this at home"
-        puts "2. I can only do this at work"
-        puts "3. I can do this anywhere but work"
-        puts "4. I can do this anywhere at all"
+        puts " 1. I can only do this at home"
+        puts " 2. I can only do this at work"
+        puts " 3. I can do this anywhere but work"
+        puts " 4. I can do this anywhere at all"
         puts ""
         puts ""
         puts "Please enter a number."
         self.new_possibility_required_location = gets.strip
+        self.quit(self.new_possibility_required_location)
         create_new_possibility
-
       else
       self.activity = Activity.create(status: "accepted", user_id: self.user.id, possibility_id: self.possibility.id)
       self.complete_activity
@@ -313,6 +315,7 @@ class Cli
     puts ""
     puts ""
     self.suggest_another = gets.strip
+    self.quit(self.suggest_another)
     self.continue?
 
 
@@ -332,7 +335,7 @@ class Cli
       puts ""
       puts ""
       self.completed_activity = gets.strip
-
+      self.quit(self.completed_activity)
       if self.completed_activity == "1"
         self.activity.status = "accepted and completed"
         self.activity.save
@@ -349,6 +352,7 @@ class Cli
         puts ""
         puts ""
         self.suggest_another = gets.strip
+        self.quit(self.suggest_another)
         self.continue?
       elsif self.completed_activity == "2"
         self.activity.status = "accepted but not completed"
@@ -357,16 +361,18 @@ class Cli
         puts ""
         puts "Would you like us to suggest a new possibility?"
         puts ""
-        puts ""
         puts "1. Yes, give me more."
         puts "2. No, I've had enough. Let's quit the program."
         puts ""
         puts ""
         self.suggest_another = gets.strip
+        self.quit(self.suggest_another)
         self.continue?
       end
     end
   end
+
+#------helper methods for CLI --------------------------------------
 
 
   def continue?
@@ -385,11 +391,32 @@ class Cli
     puts ""
     puts "Rate this activity on a scale of 1 through 5."
     user_response = gets.strip
+    self.quit(user_response)
     if user_response == ""
       puts "We missed that."
       get_rating
     end
     self.activity.rate(user_response)
   end
+
+#-----quit text and exit function
+  def quit(user_input)
+    if user_input == "quit"
+      puts ""
+      puts "Thank you for using UPTIME!"
+      puts "Have a fantastic day!"
+      puts ""
+      exit
+    end
+  end
+
+#-----what was that text
+
+  def what_was_that?
+    puts "I didn't catch that."
+    puts "Would you please try a different answer."
+  end
+
+
 
 end
